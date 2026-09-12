ee24efd826ca68f44a7d957d1fa6ee0733598672
Simon McGuirk <5030092+simonmcguirk@users.noreply.github.com>
2026-09-12 10:11:54 +0100

    Create iOS project

---
+++ MealDelivery/MealDelivery/MealDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDeliveryApp.swift
+//  MealDelivery
+//
+//  Created by Simon McGuirk on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MealDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
